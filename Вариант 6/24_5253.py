63933844cc606f0b86acf6159354b8a5ad4b7337
UnknownEvan <i.v.bachantcev@uspu.su>
2022-06-19 14:22:40 +0500

    var 6 eupdate very clever

--- Вариант 6/24_5253.py
+++ Вариант 6/24_5253.py
@@ -3,8 +3,8 @@ with open('24-208.txt') as f:
 q = s.split('2022')
 print(q)
 max_len = 0
-for i in range(1, len(q)-3):
-    c = len(q[i - 1] + q[i] + q[i + 1] + q[i + 2] + q[i + 3]) + 16
+for i in range(len(q) - 4):
+    c = len(q[i] + q[i + 1] + q[i + 2] + q[i + 3] + q[i + 4]) + 16 + 3
     if c > max_len:
         max_len = c
 print(max_len)
